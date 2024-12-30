5401334abe7c9df4ca15d726a114ee954f1b4061
devops <devops@dev.ops>
2024-12-30 16:50:31 +0900

    1230

--- main.tf
+++ main.tf
@@ -1,8 +1,29 @@
+#aws_instance
 resource "aws_instance" "example" {
   ami           = "ami-042e76978adeb8c48" #ubuntu 22.04 seoul region ami
   instance_type = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
 
-   tags  =  {
-     Name  =  "terraform-example"
-   } 
+  user_data  =  <<-EOF
+                #!/bin/bash
+                echo  "Hello, World"  >  index.html 
+                nohup  busybox  httpd  -f  -p  8080  &
+                EOF
+  user_data_replace_on_change  =  true 
+
+  tags  =  { 
+    Name  =  "terraform-example"
+  } 
+}
+
+#aws_security_group
+resource  "aws_security_group" "instance"  {
+  name  =  "terraform-example-instance" 
+  
+  ingress  { 
+    from_port    =  8080 
+    to_port      =  8080
+    protocol     =  "tcp"
+    cidr_blocks  =  [ "0.0.0.0/0" ] 
+  }
 }
